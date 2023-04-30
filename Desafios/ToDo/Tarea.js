315c628487a966e63ee1953e7e726ed581b6ac33
BMateo <75537669+NarMaroth@users.noreply.github.com>
2023-04-29 21:58:03 -0300

    Add ToDo Files

---
+++ Desafios/ToDo/Tarea.js
@@ -0,0 +1,52 @@
+
+class Tarea{
+    constructor(descripcion, container){
+        this.descripcion = descripcion
+        this.container = container
+        this.input = this.inputDescripcion()
+        this.divTarea = this.createDiv() 
+    }
+
+    inputDescripcion(){
+        let input = document.createElement("input")
+        input.classList.add("item-input")
+        input.value = this.descripcion
+        input.disabled = true
+        return input
+    }
+
+    botonEditar(){
+        let btn = document.createElement("button")
+        btn.classList.add("boton-editar")
+        btn.textContent = "🔒"
+
+        btn.addEventListener("click",_=>{
+            this.input.disabled = !this.input.disabled
+        })
+
+        return btn
+    }
+
+    botonRemover(){
+        let btn = document.createElement("button")
+        btn.classList.add("boton-remover")
+        btn.textContent = "🗑️"
+
+        btn.addEventListener("click", _=>{
+            this.container.removeChild(this.divTarea)
+        })
+
+        return btn
+    }
+
+    createDiv(){
+        let divTarea = document.createElement("div")
+        divTarea.classList.add("item")
+
+        divTarea.appendChild(this.input)
+        divTarea.appendChild(this.botonEditar())
+        divTarea.appendChild(this.botonRemover())
+
+        this.container.appendChild(divTarea)
+    }
+}
